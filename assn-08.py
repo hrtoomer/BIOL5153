7765e40fa3544b806d1088b427f1fcc491c31259
hrtoomer <47603653+hrtoomer@users.noreply.github.com>
2019-04-24 07:22:03 -0500

    Update assn-08.py

--- assn-08.py
+++ assn-08.py
@@ -7,19 +7,28 @@
 # assn_08
 
 import re
-concert = open("the_concert.txt", 'r')
+concert = open("the_concert.txt", 'r') #the concert text file
 file_contents =concert.read()
 print(file_contents)
 
 
 # In[28]:
 
-# print all matches that contain "ath"
-x = re.findall("ath", file_contents)
+# Solution 1 (my fav) - print all matches that contain "ath"
+x = re.findall("\w+ath\w+", file_contents)
+
 print(x)
+print("There are " + str(len(x)) + " forms of Katherine in this story")
 
 
 # In[ ]:
 
+# Solution 2 - Basic. if matches are found, print "Found a match"
+# This only works if you are interested in finding a match, not if you want the # or list 
+match = re.search("\w+ath\w+", file_contents)
 
+if match:
+    print("Found a match:", match.group())
+else:
+    print ("No match:", match)
 
