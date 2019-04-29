7688476ad32b19520306b7a52a69d4ad646fa195
hrtoomer <47603653+hrtoomer@users.noreply.github.com>
2019-04-29 10:27:54 -0500

    Update parseGFF.py

--- parseGFF.py
+++ parseGFF.py
@@ -9,6 +9,9 @@
 # Import SeqIO for the win
 from Bio import SeqIO
 import argparse
+import csv
+import re
+from collections import defaultdict
 
  def get_args():
 	# create an argument parser object
@@ -73,33 +76,72 @@ def rev_comp(args, genome):
             print(exon.reverse_complement())
         return rev	
 
-def CDS_intron(args, genome):
-    gff=open(args.gff_file)
-    gc_content=[]
-    for line in gff:
-        line = line.rstrip('\n')
-        fields = line.split('\t') #list the categories of the gff file by the splitting where the tab character is
-        start = int(fields[3]) # start
-        end = int(fields[4]) # stop
-        exon = genome.seq[start -1:end -1]  #create an individual substring, start from 0 position
-        feature = str(fields[2])
+def parse_gff(args, genome):
+    coding_seqs = defaultdict(dict)
+    with open(args.gff_file, 'r') as gff:
+        reader = csv.reader(gff, delimiter='\t')
+        for line in reader:
+            if not line:
+                continue
+            else:
+                    seqname=line[0]
+                    source=line[1]
+                    feature=line[2]
+                    start=int(line[3]) -1
+                    end=int(line[4]) -1
+                    score=line[5]
+                    strand=line[6]
+                    frame=line[7]
+                    attribute=line[8]
+
+        # reverse complement the feature if necessary
+
         if(feature == 'CDS'):
             # split the attributes field into its separate parts, to get the gene info
             exon_info = attributes.split(':')
 
             print(exon_info)
-
+	
             # extract the gene_name
             gene_name = fields[0].split()[1]
+	
+            if len(exon_info[0].split()) >2:
+                # extract the exon number
+                exon_number = exon_info[0].split()[-1]
+                #print(gene_name, exon_number)
+
+                if gene_name in coding_seqs:
+                    # store the coding sequence for this exon
+                else:
+                    # first time encountering this gene, so declare the dictionary for it
+                    coding_seqs[gene_name] = {}
+
+                    # store the coding sequence for this exon
+                    coding_seqs[gene_name][exon_number] = feature_sequence
+            else:
+                continue
+
+    # done reading GFF file, loop over coding_seqs to print the CDS sequences
+    for gene, exons in coding_seqs.items():
+        # new variables
+        # make a variable that will hold the concatenated sequences
+        cds_for_this_gene =''
+        # print the FASTA header for this gene ( si that it prints citrullus lanatus and then gene)
+        print('>' + gene.replace('_', '_') + '_' + gene)
+        for exon_num, exon_seq in sorted(exons.items()):
+            cds += exon_seq
+            print(exon_seq, end='')
+		
+		
 
             # test whether there is or isn't an entry in index 2, which holds the value 'exon'
             # for genes that have introns. If there is no value to index 2, then the gene doesn't
             # have an intron, and we can just print it
-            if len(fields[0].split()[2]) > 2:
-                print('This gene has an intron')
-            else:
-                print('>' + line[0].replace('_', '_') + '_' + gene_name)
-                print(feature_sequence)  
+          #  if len(fields[0].split()[2]) > 2:
+           #     print('This gene has an intron')
+          #  else:
+           #     print('>' + line[0].replace('_', '_') + '_' + gene_name)
+            #    print(feature_sequence)  
 
 
 def main():
